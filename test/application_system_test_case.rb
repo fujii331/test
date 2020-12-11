e0b7c38d0b7b818a97ba3c43a73193afec926f30
naoto613 <fujii140@gmail.com>
2020-12-11 23:54:11 +0900

    hoo

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 # test
+#uuu
+#uuu
+#iii
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
